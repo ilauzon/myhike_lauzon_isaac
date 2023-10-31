d3aa77cc6dd24f8b9ab02d6e407a2801912ce262
ilauzon <ilauzon@my.bcit.ca>
2023-10-31 11:51:58 -0700

    added firestore

--- scripts/firebaseAPI_template.js
+++ scripts/firebaseAPI_template.js
@@ -2,12 +2,13 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
+    
+    apiKey: "AIzaSyAk-f0UCgDF-Hsfem1dKdxw7ZWt5MDxqQA",
+    authDomain: "comp1800-a3789.firebaseapp.com",
+    projectId: "comp1800-a3789",
+    storageBucket: "comp1800-a3789.appspot.com",
+    messagingSenderId: "649524194613",
+    appId: "1:649524194613:web:65e149acb40a64f9f7307c"
 };
 
 //--------------------------------------------
